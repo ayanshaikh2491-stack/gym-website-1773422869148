900786da55b52f49fae43d5f3d2366cbc1e08b7d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:57:54 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422870428/pages/classes.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const Classes = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Head>
+        <title>Classes</title>
+        <meta name="description" content="Classes" />
+        <meta name="keywords" content="classes" />
+      </Head>
+      <h1>Classes</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
+        nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+};
+
+export default Classes;
