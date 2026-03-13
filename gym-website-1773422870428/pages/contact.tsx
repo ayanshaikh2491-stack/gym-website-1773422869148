d96421528afdab00c3d4d3ecf38f91333a8a5f3e
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:57:54 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422870428/pages/contact.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const Contact = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Head>
+        <title>Contact Us</title>
+        <meta name="description" content="Contact Us" />
+        <meta name="keywords" content="contact" />
+      </Head>
+      <h1>Contact Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
+        nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
